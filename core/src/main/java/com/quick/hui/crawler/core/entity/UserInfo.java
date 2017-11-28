d0887f7f32572bbadf01575d291fbcacf2b3e131
袁江 <1107678466@qq.com>
2017-11-28 18:57:45 +0800

    []

---
+++ core/src/main/java/com/quick/hui/crawler/core/entity/UserInfo.java
@@ -0,0 +1,35 @@
+package com.quick.hui.crawler.core.entity;
+
+/**
+ * Created by yuanj on 2017/11/28.
+ */
+
+public class UserInfo {
+  private String user_id;
+  private String name;
+  private Boolean response;
+
+  public String getUser_id() {
+    return user_id;
+  }
+
+  public void setUser_id(String user_id) {
+    this.user_id = user_id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Boolean getResponse() {
+    return response;
+  }
+
+  public void setResponse(Boolean response) {
+    this.response = response;
+  }
+}
