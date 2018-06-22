b6e208f19560c46176d32259592949474c163e70
袁江 <1107678466@qq.com>
2018-06-22 19:30:42 +0800

    【UPDATE】新增取消token操作

--- src/main/java/com/scheduled/ScheduledThread.java
+++ src/main/java/com/scheduled/ScheduledThread.java
@@ -29,7 +29,7 @@ public class ScheduledThread {
 
   private static List<ThreadResult> threadResults = Lists.newArrayList();
 
-  private static String version="1.2";
+  private static String version="1.3";
 
   public static List<ThreadResult> getThreadResults() {
     return threadResults;
